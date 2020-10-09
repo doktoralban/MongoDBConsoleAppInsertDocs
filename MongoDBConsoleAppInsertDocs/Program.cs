82488cb8e86c5462a92214c56cf1e34b4e482f56
Aydın Türker <doktoralban@outlook.com>
2020-10-09 14:43:34 +0300

    Add project files.

---
+++ MongoDBConsoleAppInsertDocs/Program.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using System;
+
+namespace MongoDBConsoleAppInsertDocs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbClient = new MongoClient("mongodb://127.0.0.1:27017");
+
+            IMongoDatabase db = dbClient.GetDatabase("MyDatabase");
+
+            var cars = db.GetCollection<BsonDocument>("Person");
+
+            var doc = new BsonDocument
+            {
+                {"Name", "Name05 Surname05"},
+                {"SiblingCount", 0},
+                {"DateOfBirth",new BsonDateTime(1987-12-11) }
+            };
+
+            cars.InsertOne(doc);
+
+            Console.Read();
+        }
+    }
+}
